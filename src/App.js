89b6ee3dbe638dd101bd68b3236e1dc3274110f9
sampieters <sampieters@MacBook-Pro-van-Sam.local>
2024-12-29 20:12:43 +0100

    added links for socials plus background

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 // App.js
 import React from 'react';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 //import Sidebar from './Sidebar';
-import Content from './components/Content';
 import Dock from './components/Dock';
 import './App.css';
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event) => {
+    setMousePosition({
+      x: event.clientX,
+      y: event.clientY,
+    });
+  };
+
+
   return (
-    <div className="App">
+    <div className="App" onMouseMove={handleMouseMove}>
+      <div className="dynamic-background" style={{
+        backgroundPositionX: `${mousePosition.x / 50}px`,
+        backgroundPositionY: `${mousePosition.y / 50}px`,
+      }} />
       <Navbar />
-      <div className="main-layout">
+      <div className='content'>
+        <div className="main-layout">
+        </div>
+        <Dock />
       </div>
-      <Dock />
     </div>
   );
 }
--- src/components/Dock.js
+++ src/components/Dock.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './Dock.css';
 
+import facebook from '../../src/static/facebook.png'
+import instagram from '../../src/static/instagram.png'
+import linkedin from '../../src/static/linkedin.png'
+
+
 export default function Dock() {
   return (
     <div className="dock">
-      <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-        <img src="facebook-icon.png" alt="Facebook" className="dock-icon" />
+      <a href="https://www.facebook.com/sam.pieters.77" target="_blank" rel="noopener noreferrer">
+        <img src={facebook} alt="LinkedIn" className="dock-icon" />
       </a>
-      <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-        <img src="instagram-icon.png" alt="Instagram" className="dock-icon" />
+      <a href="https://www.instagram.com/sam_pieters/" target="_blank" rel="noopener noreferrer">
+        <img src={instagram} alt="LinkedIn" className="dock-icon" />
       </a>
-      <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-        <img src="linkedin-icon.png" alt="LinkedIn" className="dock-icon" />
+      <a href="https://www.linkedin.com/in/sam-pieters-462435288/" target="_blank" rel="noopener noreferrer">
+        <img src={linkedin} alt="LinkedIn" className="dock-icon" />
       </a>
     </div>
   );
